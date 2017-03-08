5982276abf493926f5e9aa6e4e09f460dca0257f
DannyJamieson <danj1066@hotmail.co.uk>
2017-03-08 17:08:21 +0000

    Meeting minutes

--- Game/Group Game/Assets/Scripts/HighScoreControl.cs
+++ Game/Group Game/Assets/Scripts/HighScoreControl.cs
@@ -7,7 +7,18 @@ public class HighScoreControl : MonoBehaviour {
     {
         DontDestroyOnLoad(transform.gameObject);
     }
-    public      int[] HighScores = { 600, 600, 600, 600, 600 };
+    public int[] HighScores = new int[5];
+
+    private void Start()
+    {
+        if (HighScores[0] == 0)
+        {
+            for(int i=0; i<HighScores.Length; i++)
+            {
+                HighScores[i] = 600;
+            }
+        }
+    }
 
     public void checkTime(int Level, int Time)
     {
